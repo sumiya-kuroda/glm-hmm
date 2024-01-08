Fix wrong dim of y

1_preprocess_data/dmdm/2_create_design_mat.py
@@ -54,20 +54,20 @@ def main(dname, *, req_num_sessions = 30, num_folds=5):
         sess_counter = 0
         for eid in animal_eid_dict[animal]:
 
-            animal, unnormalized_inpt, outcome, session, reactiontimes, stimT = \
+            animal, unnormalized_inpt, y, session, reactiontimes, stimT = \
                 get_all_unnormalized_data_this_session(
                     eid, dmdm_data_path)
             
             if sess_counter == 0:
                 animal_unnormalized_inpt = np.copy(unnormalized_inpt)
-                animal_y = np.copy(outcome)
+                animal_y = np.copy(y)
                 animal_session = session
                 animal_rt = np.copy(reactiontimes)
                 animal_stimT = np.copy(stimT)
             else:
                 animal_unnormalized_inpt = np.vstack(
                     (animal_unnormalized_inpt, unnormalized_inpt))
-                animal_y = np.concatenate((animal_y, outcome))
+                animal_y = np.concatenate((animal_y, y))
                 animal_session = np.concatenate((animal_session, session))
                 animal_rt = np.concatenate((animal_rt, reactiontimes))
                 animal_stimT = np.concatenate((animal_stimT, stimT))

1_preprocess_data/dmdm/preprocessing_utils.py
@@ -44,11 +44,12 @@ def get_all_unnormalized_data_this_session(eid, path_to_dataset):
                                                          outcome,
                                                          reactiontimes,
                                                          stimT)
+    y = np.expand_dims(outcome_noref, axis=1)
     session = [session_id for i in range(changesize.shape[0])]
     # You can add some criteria here and change codes to 
     # something like changesize[trials_to_study]
 
-    return animal, unnormalized_inpt, outcome_noref, session, reactiontimes, stimT
+    return animal, unnormalized_inpt, y, session, reactiontimes, stimT
 
 def get_raw_data(eid, path_to_dataset):
     print(eid)